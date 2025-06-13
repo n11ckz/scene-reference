dbbd457ec600484e97824251ef0a50243c3ee329
никита² <126013251+n11ckz@users.noreply.github.com>
2025-06-13 14:03:43 +0300

    chore: rename project parts
    Renamed folder "_Examples" to "_Samples" and other parts related to samples.

--- Assets/_Examples/Scripts/Example.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace n11ckz.SceneReference.Examples
-{
-    public class Example : MonoBehaviour
-    {
-        [SerializeField] private SceneReference _sceneReference;
-    }
-}
