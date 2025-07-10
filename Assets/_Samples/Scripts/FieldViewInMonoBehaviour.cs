c999a85ac1f4626e931b1385fdf2ebc3f5571365
никита² <126013251+n11ckz@users.noreply.github.com>
2025-07-10 19:47:48 +0300

    Revert "feat: add advanced sample"
    This reverts commit 7333d57b674227e67a086d538f6d6b57ea73ef05.

---
+++ Assets/_Samples/Scripts/FieldViewInMonoBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace n11ckz.SceneReference.Samples
+{
+    public class FieldViewInMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField] private SceneReference _sceneReference;
+        [SerializeField] private List<SceneReference> _sceneReferences;
+    }
+}
